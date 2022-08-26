41456d6058a061a305141e0c6324b2d00929e5a9
nahuedev@gmail.com <nahuedev@gmail.com>
2022-08-26 19:18:55 -0300

    refactor ej_8 and ej_9

--- tp_2/ej_9.py
+++ tp_2/ej_9.py
@@ -2,17 +2,17 @@
 #Realizar un programa que imprima el número del vendedor y el salario que le corresponde en un determinado mes.
 #Se leen el número del vendedor, la cantidad de ventas que realizó y el valor total de las mismas.
 
-salario = 50000
-comision_fija = 5000
-tasa_comision_variable = 1.05
+SALARIO = 50000
+COMISION_FIJA = 5000
+TASA_COMISION_VARIABLE= 1.05
 
 numero_vendedor = input('Ingrese el numero de vendedor: ')
 ventas_del_mes = int(input('Ingrese las ventas del mes: '))
 valor_ventas_mes = int(input('Ingrese el valor total de la ventas del mes: '))
 
-comision_por_venta = ventas_del_mes * comision_fija
-comision_variable = comision_por_venta * tasa_comision_variable
+comision_por_venta = ventas_del_mes * COMISION_FIJA
+comision_variable = comision_por_venta * TASA_COMISION_VARIABLE
 
-print('Salario del mes: $', salario)
+print('Salario del mes: $', SALARIO)
 print('Comision por ventas del mes: $', comision_por_venta)
 print('Comision variable por ventas del mes: $', comision_variable)
